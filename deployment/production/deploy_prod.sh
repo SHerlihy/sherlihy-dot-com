650936e69cd78cb66fcb59e5ab928493eaffe2ab
SHerlihy <steven_herlihy@yahoo.com>
2023-12-30 02:29:17 +0000

    prod deploy with modules

--- deployment/production/deploy_prod.sh
+++ deployment/production/deploy_prod.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+rm -rf .ssh
+
+mkdir .ssh
+ssh-keygen -q -f ./.ssh/id_rsa -N '' -t rsa
+
 terraform init -input=false
 terraform destroy -auto-approve
 terraform plan -out=./tf.plan
--- deployment/production/main.tf
+++ deployment/production/main.tf
@@ -17,217 +17,86 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_vpc" "lb_test" {
+resource "aws_vpc" "sherlihy_dot_com" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "lb_test" {
-  vpc_id = aws_vpc.lb_test.id
-}
-
-locals {
-  az_names = { for idx, az_name in data.aws_availability_zones.available.names : idx => az_name }
+resource "aws_internet_gateway" "sherlihy_dot_com" {
+  vpc_id = aws_vpc.sherlihy_dot_com.id
 }
 
 resource "aws_subnet" "publics" {
-  for_each = local.az_names
+    count = 2
+  vpc_id     = aws_vpc.sherlihy_dot_com.id
+  cidr_block = "10.0.${count.index+1}.0/24"
 
-  vpc_id     = aws_vpc.lb_test.id
-  cidr_block = "10.0.${(each.key + 1) * 2}.0/24"
-
-  availability_zone = each.value
-
-  tags = {
-    az_name = each.value
-  }
-}
-
-resource "aws_subnet" "privates" {
-  for_each = local.az_names
-
-  vpc_id     = aws_vpc.lb_test.id
-  cidr_block = "10.0.${(each.key * 2) + 1}.0/24"
-
-  availability_zone = each.value
-
-  tags = {
-    az_name = each.value
-  }
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 resource "aws_route_table" "inet_gw-publics" {
-  vpc_id = aws_vpc.lb_test.id
+  vpc_id = aws_vpc.sherlihy_dot_com.id
 }
 
 resource "aws_route" "inet_gw" {
   route_table_id         = aws_route_table.inet_gw-publics.id
   destination_cidr_block = "0.0.0.0/0"
 
-  gateway_id = aws_internet_gateway.lb_test.id
+  gateway_id = aws_internet_gateway.sherlihy_dot_com.id
 }
 
 resource "aws_route_table_association" "inet_gw-publics" {
-  for_each = aws_subnet.publics
+    count = length(aws_subnet.publics)
 
-  subnet_id      = each.value.id
+  subnet_id      = aws_subnet.publics[count.index].id
   route_table_id = aws_route_table.inet_gw-publics.id
 }
 
-locals {
-  az_name-public_subnet  = { for idx, public_subnet in aws_subnet.publics : public_subnet.tags_all.az_name => public_subnet.id }
-  az_name-private_subnet = { for idx, private_subnet in aws_subnet.privates : private_subnet.tags_all.az_name => private_subnet.id }
-  az_name-subnets = { for idx, az_name in local.az_names : az_name => {
-    pubId = local.az_name-public_subnet[az_name]
-    pvtId = local.az_name-private_subnet[az_name]
-    }
-  }
+resource "aws_key_pair" "sherlihyDotCom_instance" {
+  key_name   = "sherlihyDotCom-instance"
+  public_key = file("./.ssh/id_rsa.pub")
 }
 
-module "nat_access" {
-  for_each = local.az_name-subnets
-  source   = "../modules/nat_access"
+module "web_server_instance" {
+    count = length(aws_subnet.publics)
+  source = "../modules/image_instance"
 
-  vpc_id = aws_vpc.lb_test.id
+  vpc_id    = aws_vpc.sherlihy_dot_com.id
+  subnet_id      = aws_subnet.publics[count.index].id
 
-  pub_subnet_id = each.value.pubId
+  ingress_port_list = tolist([80, 443])
 
-  pvt_subnet_id = each.value.pvtId
+    key_name = aws_key_pair.sherlihyDotCom_instance.key_name
 
-  depends_on = [aws_internet_gateway.lb_test]
-}
-
-resource "aws_security_group" "web_page-lb_test" {
-  name   = "web_page-lb_test"
-  vpc_id = aws_vpc.lb_test.id
-}
-
-resource "aws_security_group_rule" "ingress_http" {
-  type              = "ingress"
-  security_group_id = aws_security_group.web_page-lb_test.id
-
-  from_port   = "80"
-  to_port     = "80"
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "egress_all_ports" {
-  type              = "egress"
-  security_group_id = aws_security_group.web_page-lb_test.id
-
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-data "aws_ami" "sherlihy_dot_com-lb_test" {
-  most_recent = true
-  owners      = ["111644099040"]
-
-  filter {
-    name   = "name"
-    values = ["sherlihy-dot-com/*"]
-  }
-}
-
-resource "aws_instance" "sherlihy_dot_com-pvts" {
-  for_each      = aws_subnet.privates
-  ami           = data.aws_ami.sherlihy_dot_com-lb_test.id
-  instance_type = "t2.micro"
-
-  subnet_id = each.value.id
-
-  vpc_security_group_ids = [aws_security_group.web_page-lb_test.id]
-
-  user_data = file("../configuration_scripts/web_server-init.sh")
-}
-
-resource "aws_security_group" "inet_access" {
-  name   = "inetAccess"
-  vpc_id = aws_vpc.lb_test.id
-}
-
-resource "aws_security_group_rule" "inet_ingress_https" {
-  type              = "ingress"
-  security_group_id = aws_security_group.inet_access.id
-
-  from_port   = "443"
-  to_port     = "443"
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "inet_ingress_http" {
-  type              = "ingress"
-  security_group_id = aws_security_group.inet_access.id
-
-  from_port   = "80"
-  to_port     = "80"
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "inet_egress" {
-  type              = "egress"
-  security_group_id = aws_security_group.inet_access.id
-
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+    init_file_path = "../configuration_scripts/web_server-init.sh"
 }
 
 locals {
-  pub_subnets-ids = [for publicInstance in aws_subnet.publics : publicInstance.id]
+    subnet_ids = [for subnet in aws_subnet.publics : subnet.id]
+    instance_ids = [for instance in module.web_server_instance : instance.instance_id]
 }
 
-resource "aws_lb" "lb_test" {
-  name               = "lb-test"
-  internal           = false
-  load_balancer_type = "application"
+module "lb_tls_proxy" {
+    source = "../modules/lb_tls_proxy"
 
-  security_groups = [aws_security_group.inet_access.id]
-  subnets         = local.pub_subnets-ids
+  vpc_id    = aws_vpc.sherlihy_dot_com.id
+
+  ingress_port_list = tolist([80, 443])
+
+    instance_ids = local.instance_ids
+    subnet_ids = local.subnet_ids
+
+    target_port = 80
+    TLS_cert_arn = var.TLS_cert_arn
 }
 
-resource "aws_lb_target_group" "privates" {
-  name     = "privates-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.lb_test.id
-}
-
-resource "aws_lb_target_group_attachment" "privates" {
-  for_each = aws_instance.sherlihy_dot_com-pvts
-
-  target_group_arn = aws_lb_target_group.privates.arn
-  target_id        = each.value.id
-  port             = 80
-}
-
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.lb_test.arn
-  port              = 443
-  protocol          = "HTTPS"
-
-  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn = var.TLS_cert_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.privates.arn
-  }
-}
-
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "sherlihy_dot_com" {
   zone_id = var.zone_id
   name    = var.domain_name
   type    = "A"
 
   alias {
-    name                   = aws_lb.lb_test.dns_name
-    zone_id                = aws_lb.lb_test.zone_id
+    name                   = module.lb_tls_proxy.dns_name
+    zone_id                = module.lb_tls_proxy.zone_id
     evaluate_target_health = true
   }
 }
