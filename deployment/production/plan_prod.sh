dfadc48aa0f22f408973e514d5648e40f26544a1
SHerlihy <steven_herlihy@yahoo.com>
2024-01-03 17:57:13 +0000

    correct plan script

--- deployment/production/plan_prod.sh
+++ deployment/production/plan_prod.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+terraform init -input=false
 terraform destroy -auto-approve
 
 rm -rf .ssh
@@ -10,10 +11,9 @@ ssh-keygen -q -f ./.ssh/id_rsa -N '' -t rsa
 find ../../dist -type f > ./content_s3/dist_file_paths.txt 
 awk -f ./content_s3/dist_paths_to_list.awk ./content_s3/dist_file_paths.txt > ./plan_vars.tfvars
 
-echo -e "\ndomain_name = \"herlihy.com\"" >> ./plan_vars.tfvars
+echo -e "\ndomain_name = \"sherlihy.com\"" >> ./plan_vars.tfvars
 #echo -e "\ndomain_name = \"stagestaging.click\"" >> ./plan_vars.tfvars
 
-terraform init -input=false
 terraform plan -var-file=./plan_vars.tfvars -out=./tf.plan
 
 exit
