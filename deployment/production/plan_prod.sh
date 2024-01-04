c58607835bf03246db9037d0461e1709be7df7f6
SHerlihy <steven_herlihy@yahoo.com>
2024-01-04 01:13:38 +0000

    succeed in manual deploy

--- deployment/production/plan_prod.sh
+++ deployment/production/plan_prod.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-terraform init -input=false
-terraform destroy -auto-approve
-
 rm -rf .ssh
 
 mkdir .ssh
@@ -14,6 +11,9 @@ awk -f ./content_s3/dist_paths_to_list.awk ./content_s3/dist_file_paths.txt > ./
 echo -e "\ndomain_name = \"sherlihy.com\"" >> ./plan_vars.tfvars
 #echo -e "\ndomain_name = \"stagestaging.click\"" >> ./plan_vars.tfvars
 
+terraform init -input=false
+
+terraform destroy -var-file=./plan_vars.tfvars -auto-approve 
 terraform plan -var-file=./plan_vars.tfvars -out=./tf.plan
 
 exit
