4e4d321d062d75aade994e01ccb0c8041b1c88ce
SHerlihy <steven_herlihy@yahoo.com>
2024-01-23 03:20:02 +0000

    impove to readme script

--- deployment/infrastructure_diagrams/diagrams_to_readme.cjs
+++ deployment/infrastructure_diagrams/diagrams_to_readme.cjs
@@ -1,5 +1,6 @@
 const {globSync} = require("glob");
 const {execSync} = require('child_process');
+const fs = require('fs')
 
 const htmlNames = globSync("**/*.html", {})
 const fileNames = []
@@ -16,28 +17,43 @@ execSync('awk -i inplace -f ./delete_infrastructure_diagrams.awk ../../README.md
 
 const mdNames = globSync("**/*.md", {})
 
+const diagramsStr = []
 for (let i=0; i<mdNames.length; i++) {
-    const mdStr = execSync(`awk 'BEGIN {ORS="\\\\n"};!/^\s?\`\`\`/ {print}' ${mdNames[i]}`, { encoding: 'utf-8' })
+    const diaMD = fs.readFileSync(mdNames[i]).toString().trim();
+    const diaSpcSplit = diaMD.split("\n")
+    diaSpcSplit.shift()
+    diaSpcSplit.pop()
 
+    const diaSpcJoin = diaSpcSplit.join("\\n")
+
+    const diaCommaSplit = diaSpcJoin.split("\"")
+    const diaCommaJoin = diaCommaSplit.join("\\\"")
+
+    diagramsStr.push(diaCommaJoin)
+}
+
+execSync(`awk -i inplace \
+    'BEGIN {toPrint = 0};\
+    /^\\s*---/ {toPrint = 0};\
+    {if (toPrint == 0) print};\
+    /^## Infrastructure Diagrams.?$/ {toPrint = 1}' \
+    ../../README.md`,
+    { encoding: 'utf-8' }
+)
+
+for (let i=0; i<diagramsStr.length; i++) {
     execSync(`awk -i inplace \
-        '{print};\
-        /^## Infrastructure Diagrams.?$/ {print "\`\`\`mermaid"}'\
+        'BEGIN {toAdd=0};\
+        {print};\
+        /^## Infrastructure Diagrams.?$/ {if (toAdd == 0) print "\`\`\`mermaid";toAdd=1}' \
         ../../README.md`,
         { encoding: 'utf-8' }
     )
-
+    
     execSync(`awk -i inplace \
-        'BEGIN {toAdd=1};\
+        'BEGIN {toAdd=0};\
         {print};\
-        /\`\`\`mermaid/ {if (toAdd == 1) print "\`\`\`";toAdd=0}' \
-        ../../README.md`,
-        { encoding: 'utf-8' }
-    )
-
-    execSync(`awk -i inplace \
-        'BEGIN {toAdd=1};\
-        {print};\
-        /\`\`\`mermaid/ {if (toAdd == 1) print "${mdStr}";toAdd=0}' \
+/\`\`\`mermaid/ {if (toAdd == 0) print "${diagramsStr[i]}"; print "\`\`\`"; toAdd=1}' \
         ../../README.md`,
         { encoding: 'utf-8' }
     )
