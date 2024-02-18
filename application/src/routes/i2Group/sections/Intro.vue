5273b12f70cbabbe8f1483f7adda1e8f8b01ba2f
SHerlihy <steven_herlihy@yahoo.com>
2024-02-18 11:29:54 +0000

    style i2 web

--- application/src/routes/i2Group/sections/Intro.vue
+++ application/src/routes/i2Group/sections/Intro.vue
@@ -5,7 +5,12 @@
 <template>
     <Introduction>
         <template #image>
-            <img src="/public/crimeWeb.png" />
+            <div>
+            <img src="/public/spiderweb.png" />
+            <img src="/public/crimes1.png" class="drop_bounce1"/>
+            <img src="/public/crimes2.png" class="drop_bounce2"/>
+            <img src="/public/crimes3.png" class="drop_bounce3"/>
+            </div>
         </template>
         <template #text>
             <p >
@@ -14,3 +19,81 @@
         </template>
     </Introduction>
 </template>
+
+<style scoped>
+div {
+    position: relative;
+    height: 450px;
+    width: 450px;
+}
+
+img {
+    position: absolute;
+}
+
+.drop_bounce1{
+    animation: drop-bounce 1000ms, drop-sway-a 1000ms;
+}
+
+.drop_bounce2{
+    animation: drop-bounce 1200ms, drop-sway-b 1200ms;
+}
+
+.drop_bounce3{
+    animation: drop-bounce 800ms;
+}
+
+@keyframes drop-bounce {
+    0% {
+        top: -100px;
+    }
+
+    33% {
+        top: 5px;
+    }
+
+    69% {
+        top: -10px;
+    }
+
+    100% {
+        top: 0px;
+    }
+}
+
+@keyframes drop-sway-a {
+    0% {
+        left: 0px;
+    }
+
+    33% {
+        left: -2px;
+    }
+
+    69% {
+        left: 2px;
+    }
+
+    100% {
+        left: 0px;
+    }
+}
+
+@keyframes drop-sway-b {
+    0% {
+        left: 0px;
+    }
+
+    33% {
+        left: 2px;
+    }
+
+    69% {
+        left: -2px;
+    }
+
+    100% {
+        left: 0px;
+    }
+}
+</style>
