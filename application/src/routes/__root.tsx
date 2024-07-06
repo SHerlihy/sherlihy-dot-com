84aa4f36d3cc967031860b121dbb4bb6f93df3d6
SHerlihy <steven_herlihy@yahoo.com>
2024-07-06 09:03:54 +0100

    add app styles to main

--- application/src/routes/__root.tsx
+++ application/src/routes/__root.tsx
@@ -1,8 +1,5 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 
-import css from '../shared/styles/App.module.css'
-css
-
 export const Route = createRootRoute({
   component: () => (
     <>
