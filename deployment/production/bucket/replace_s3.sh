42c713ab40a88222dbbf6c1b1f4c3de0aecefd90
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2025-03-12 11:56:15 +0000

    Tailwind styling (#41)
    * upgrate vite to v6
    
    * example of react tailwing and tanstack router working
    
    * adopt prev file structure
    
    * tailwind added to tech stack
    
    * change tab info
    
    * deploy issue 1
    
    * script not found

--- deployment/production/bucket/replace_s3.sh
+++ deployment/production/bucket/replace_s3.sh
@@ -4,8 +4,13 @@ awk '{print $0}' ./upload_s3/init_vars.tfvars > ./upload_s3/vars.tfvars
 terraform -chdir=./init_roles output >> ./upload_s3/vars.tfvars
 terraform -chdir=./s3_bucket output >> ./upload_s3/vars.tfvars
 
+
 cd ./upload_s3
+
+echo "before replace files"
+pwd
 ./replace_files.sh
+echo "after replace files"
 
 STATUS=$?
 if [ $STATUS -gt 0 ]
