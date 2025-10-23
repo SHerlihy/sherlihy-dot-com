39af67267960e473a5c4cda8fe6a29b53ba93841
SHerlihy <steven_herlihy@yahoo.com>
2025-10-23 09:25:49 +0100

    conditionally render contact info

--- application/src/shared/components/ControlBar.tsx
+++ application/src/shared/components/ControlBar.tsx
@@ -36,10 +36,41 @@ flex
 ${isDesktop ? 'flex-row' : 'flex-col'}
 `}
             >
-                <p>+44 7354430588</p>
-                {isDesktop && <p>&nbsp;|&nbsp;</p>}
-                <p> steven_herlihy@yahoo.com</p>
+                {isDesktop && <ContactInfoDesktop/>}
+                {!isDesktop && <ContactInfoMobile/>}
             </div>
+        </div>
+    )
+}
+
+const PHONE_NO = "+44 7354430588"
+const EMAIL = "steven_herlihy@yahoo.com"
+
+function ContactInfoDesktop() {
+    return (
+        <div>
+            <p>{PHONE_NO}</p>
+            <p>&nbsp;|&nbsp;</p>
+            <p>{EMAIL}</p>
+        </div>
+    )
+}
+
+function ContactInfoMobile() {
+    return (
+        <div className={`grid grid-cols-2 grid-rows-2`}>
+            <p className={`row-start-1 row-end-2 col-start-1 col-end-2`}>
+                {PHONE_NO}
+            </p>
+            <p className={`row-start-1 row-end-2 col-start-2 col-end-3`}>
+                copy
+            </p>
+            <p className={`row-start-2 row-end-3 col-start-1 col-end-2`}>
+                {EMAIL}
+            </p>
+            <p className={`row-start-2 row-end-3 col-start-2 col-end-3`}>
+                copy
+            </p>
         </div>
     )
 }
