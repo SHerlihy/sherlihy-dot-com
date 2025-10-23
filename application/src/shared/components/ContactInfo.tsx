8df1f3b5442db750bbe4b34205cd3b8cb13a120d
SHerlihy <steven_herlihy@yahoo.com>
2025-10-23 07:41:32 +0100

    add icons to nav menu

--- application/src/shared/components/ContactInfo.tsx
+++ application/src/shared/components/ContactInfo.tsx
@@ -3,13 +3,15 @@ import useIsDesktop from "../hooks/useIsDesktop"
 function ContactInfo({style}:{style:string}) {
     const isDesktop = useIsDesktop()
     return (
-        <div className={`pb-4 flex justify-center items-center
+        <div className={`flex justify-center items-center
 ${isDesktop ? 'flex-row' : 'flex-col'}
 ${style}
 `}>
+<div>
             <p>+44 7354430588</p>
             {isDesktop && <p>&nbsp;|&nbsp;</p>}
             <p> steven_herlihy@yahoo.com</p>
+</div>
         </div>
     )
 }
--- application/src/shared/layouts/MobileLayout.tsx
+++ application/src/shared/layouts/MobileLayout.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, ReactNode, SetStateAction, useState } from "react";
 import ContactInfo from "../components/ContactInfo";
+import { MenuIcon, BackIcon } from "../icons";
 
 function MobileLayout({ content, navigate }: { content: ReactNode, navigate: ReactNode }) {
     const [isNav, setIsNav] = useState(false)
@@ -7,17 +8,13 @@ function MobileLayout({ content, navigate }: { content: ReactNode, navigate: Rea
     return (
         <main className={`h-full flex flex-col`}>
             <div className={`grow-1 grid gap-4 grid-rows-9 min-h-0 min-w-0`}>
-                <div className={`row-start-1 row-end-2`}>
-                    <ContactInfo />
-                    <hr />
+                <div className={`flex justify-center row-start-1 row-end-2`}>
+<ControlBar isNav={isNav} setIsNav={setIsNav} />
                 </div>
-                <div className={`flex flex-col row-start-2 row-end-9`}>
+                <div className={`flex flex-col row-start-2 row-end-10`}>
                     {isNav && navigate}
                     {!isNav && content}
                     <hr />
-                </div>
-                <div className={`flex justify-center row-start-9 row-end-10`}>
-<ControlBar isNav={isNav} setIsNav={setIsNav} />
                 </div>
             </div>
         </main>
@@ -32,8 +29,8 @@ return (
                         className={`col-start-1 col-end-2
                         p-4 hover:cursor-pointer`}
                         onClick={() => setIsNav((prev) => !prev)}>
-                        {isNav && <p>View</p>}
-                        {!isNav && <p>Navigate</p>}
+                        {isNav && <BackIcon/>}
+                        {!isNav && <MenuIcon/>}
                     </button>
                     <ContactInfo style={`col-start-2 col-end-5`}/>
 </div>
