fe37192c5ed4aa4bd982c444665383fccba94d0f
SHerlihy <steven_herlihy@yahoo.com>
2025-10-25 09:37:38 +0100

    consolidate page layout

--- application/src/shared/layouts/PageLayout.tsx
+++ application/src/shared/layouts/PageLayout.tsx
@@ -1,22 +1,43 @@
-import { ReactNode } from "react"
+import { ReactNode, useContext } from "react"
 import useIsDesktop from "../hooks/useIsDesktop"
 import DesktopLayout from "./DesktopLayout"
 import MobileLayout from "./MobileLayout"
+import { NavContext } from "../context/NavContext"
+import ControlBar from "../components/ControlBar"
 
 function PageLayout({ content, navigate }: { content: ReactNode, navigate: ReactNode }) {
+    // const isDesktop = useIsDesktop()
+    //
+    // return (
+    //     <>
+    //         {isDesktop && <DesktopLayout
+    //             content={content}
+    //             navigate={navigate}
+    //         />}
+    //         {!isDesktop && <MobileLayout
+    //             content={content}
+    //             navigate={navigate}
+    //         />}
+    //     </>
+    // )
     const isDesktop = useIsDesktop()
+    const { isNav } = useContext(NavContext)
 
     return (
-        <>
-            {isDesktop && <DesktopLayout
-                content={content}
-                navigate={navigate}
-            />}
-            {!isDesktop && <MobileLayout
-                content={content}
-                navigate={navigate}
-            />}
-        </>
+        <main className={`
+${isDesktop && "p-10"}
+flex flex-col w-full h-full overflow-hidden
+`}>
+            <div>
+                <ControlBar />
+            </div>
+            <hr className={`pb-4`} />
+            <article className={`flex flex-col flex-1 overflow-hidden`}>
+                {isNav && navigate}
+                {!isNav && content}
+                <hr />
+            </article>
+        </main>
     )
 }
 
