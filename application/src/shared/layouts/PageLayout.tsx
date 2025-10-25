7b6500c54c032c50c25b8ae1c9d397933a047856
SHerlihy <steven_herlihy@yahoo.com>
2025-10-25 13:24:20 +0100

    center a div

--- application/src/shared/layouts/PageLayout.tsx
+++ application/src/shared/layouts/PageLayout.tsx
@@ -30,8 +30,10 @@ function ContentLayout({ children }: { children: ReactNode }) {
 
     if (isDesktop) {
         return (
-            <div className={`p-16 flex-1 flex flex-col justify-center`}>
-                {children}
+            <div className={`p-16 flex-1 flex flex-row justify-center align-center`}>
+                <div className={`flex flex-col justify-center align-center`}>
+                    {children}
+                </div>
             </div>
         )
     }
