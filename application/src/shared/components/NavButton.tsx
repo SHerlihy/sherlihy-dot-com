b3ddb73a62bb34e56a25a70af3449ded9bb11165
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2025-10-26 11:09:03 +0000

    Update content 10 25 (#54)
    * update home page
    
    * link text to match

--- application/src/shared/components/NavButton.tsx
+++ application/src/shared/components/NavButton.tsx
@@ -1,7 +1,8 @@
 import { Link, useLocation } from "@tanstack/react-router"
-import { useContext, type ReactNode } from "react"
+import { useCallback, useContext, useEffect, useRef, useState, type ReactNode } from "react"
 import { Route, type AllQueryOptions } from "../../routes"
 import { NavContext } from "../context/NavContext"
+import { createPortal } from "react-dom"
 
 function NavButton(props: {
     children: ReactNode
@@ -9,25 +10,26 @@ function NavButton(props: {
     queryParam?: AllQueryOptions
     pathname?: string
 }) {
-
-    const {toggleIsNav} = useContext(NavContext)
+    const { toggleIsNav } = useContext(NavContext)
     const { highlight } = Route.useSearch()
     const location = useLocation()
+
     return (
-        <Link
-            className={`flex justify-center items-center ${props.className}`}
-            to={props.pathname ? props.pathname : location.pathname}
-            search={() => ({ highlight: props.queryParam })}
-            onClick={()=>toggleIsNav()}
-        >
-            <p className={`
+            <Link
+                style={{ color: 'inherit' }}
+                className={`flex justify-center items-center ${props.className}`}
+                to={props.pathname ? props.pathname : location.pathname}
+                search={() => ({ highlight: props.queryParam })}
+                onClick={() => toggleIsNav()}
+            >
+                <p className={`
 hover:underline
 hover:duration-500
 hover:text-purple-500
 ${highlight === props.queryParam ? 'underline text-purple-500' : ''}`}>
-                {props.children}
-            </p>
-        </Link>
+                    {props.children}
+                </p>
+            </Link>
     )
 }
 
